cd6677c494403fa464a483b940b43211aff6e28a
Willmorp <0920076@utp.edu.pe>
2018-02-21 20:51:41 -0500

    clase MycowService.java

---
+++ src/pe/com/mycow/models/MycowService.java
@@ -0,0 +1,125 @@
+package pe.com.mycow.models;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MycowService {
+
+    private Connection connection;
+    private UsersEntity usersEntity;
+    private WorkplacesEntity workplacesEntity ;
+    private WorkspacesEntity workspacesEntity;
+    private ReserveSpacesEntity reservespacesEntity;
+
+    private Connection getConnection() {
+        if(connection == null) {
+            try {
+                connection = ((DataSource) InitialContext
+                        .doLookup("jdbc/MySQLDataSource2"))
+                        .getConnection();
+            } catch (NamingException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    protected UsersEntity getUsersEntity() {
+        if(getConnection() != null) {
+            if(usersEntity == null) {
+                usersEntity = new UsersEntity();
+                usersEntity.setConnection(getConnection());
+            }
+        }
+        return usersEntity;
+    }
+
+    protected WorkplacesEntity getWorkplacesEntity() {
+        if(getConnection() != null) {
+            if(workplacesEntity == null) {
+                workplacesEntity = new WorkplacesEntity();
+                workplacesEntity.setConnection(getConnection());
+            }
+        }
+        return workplacesEntity;
+    }
+
+    protected WorkspacesEntity getWorkSpacesEntity() {
+        if(getConnection() != null) {
+            if(workspacesEntity == null) {
+                workspacesEntity = new WorkspacesEntity();
+                workspacesEntity.setConnection(getConnection());
+            }
+        }
+        return workspacesEntity;
+    }
+    protected ReserveSpacesEntity getReserveSpacesEntity(){
+        if(getConnection() != null){
+            if (reservespacesEntity == null){
+                reservespacesEntity = new ReserveSpacesEntity();
+                reservespacesEntity.setConnection(getConnection());
+            }
+        }
+        return reservespacesEntity;
+    }
+
+    //********************
+    //       FindAll
+    //*********************
+    public List<User> findAllUsers() {
+        return getUsersEntity() != null ?
+                getUsersEntity().findAll() : null;
+    }
+
+    public List<Workplace> findAllWorkplaces(){
+        return getWorkplacesEntity() != null ?
+                getWorkplacesEntity().findAll() : null;
+    }
+
+    public List<Workspace> findAllWorkSpaces() {
+        return getWorkSpacesEntity() != null ?
+                getWorkSpacesEntity().findAll() : null;
+    }
+    public List<ReserveSpace> findAllReserveSpaces(){
+        return getReserveSpacesEntity() != null ?
+                getReserveSpacesEntity().findAll() : null;
+    }
+
+
+
+
+    public User findUserById(int id) {
+        return getUsersEntity() != null ?
+                getUsersEntity().findById(id) : null;
+    }
+
+    public User findUserByName(String name) {
+        return getUsersEntity() != null ?
+                getUsersEntity().findByName(name) : null;
+    }
+
+    public User createUser(String name) {
+        return getUsersEntity() != null ?
+                getUsersEntity().create(name) : null;
+    }
+
+    public boolean deleteUser(int id) {
+        return getUsersEntity() != null ?
+                getUsersEntity().delete(id) : false;
+    }
+
+    public boolean updateUser(User region) {
+        return getUsersEntity() != null ?
+                getUsersEntity().update(region) : false;
+    }
+
+
+}
