6c3d1946114f05d1d0bbdeb0e8f6b27b42a63575
iDarkcy <35429143+iDarkcy@users.noreply.github.com>
2018-02-21 21:10:24 -0500

    Create Workspace.java
    Creación de la clase espacios de trabajo.

---
+++ src/pe/com/mycow/models/Workspace.java
@@ -0,0 +1,107 @@
+package pe.com.mycow.models;
+public class Workspace {
+    private int id;
+    private int price;
+    private int capacity;
+    private String address;
+    private String description;
+    private String fhoto;
+    private Workplace workplace;
+    private User user;
+    private Spacetype spacetype;
+    
+    public workspace(){
+    }
+    
+    public Workspace(int id, int price, int capacity, String address, String description, String fhoto, Workplace workplace, User user, Spacetype spacetype) {
+        this.id = id;
+        this.price = price;
+        this.capacity = capacity;
+        this.address = address;
+        this.description = description;
+        this.fhoto = fhoto;
+        this.workplace = workplace;
+        this.user = user;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public Workspace setId(int id){
+        this.id = id;
+        return this,
+    }
+    
+     public int getPrice() {
+        return price;
+    }
+    
+    public Workspace setPrice(int price) {
+        this.price = price;
+        return this;
+    }
+    
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Workspace setCapacity(int capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public Workspace setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public Workspace setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+    
+    public String getFhoto(){
+        return fhoto;
+    }
+    
+    public Workspace setFhoto(String fhoto){
+        this.fhoto = fhoto;
+        return this;
+    }
+    
+    public Workplace getWorkplace(){ 
+        return workplace;
+    }
+
+    public Workspace setWorkplace(Workplace workplace) {
+        this.workplace = workplace;
+        return this;
+    }
+    
+    public User getUser(){ 
+        return user;
+    }
+
+    public Workspace setUser(User user) {
+        this.user = user;
+        return this;
+    }
+    
+    public Spacetype getSpacetype(){
+        return spacetype;
+    }
+    
+    public Workspace setSpacetype(Spacetype spacetype){
+        this.spacetype = spacetype;
+        return this;
+    }
+    
