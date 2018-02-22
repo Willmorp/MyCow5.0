2dc9428887548df5ddea96b5168241e3934c535a
Willmorp <0920076@utp.edu.pe>
2018-02-21 20:50:52 -0500

    completando la clase BaseEntity

--- src/pe/com/mycow/models/BaseEntity.java
+++ src/pe/com/mycow/models/BaseEntity.java
@@ -1,4 +1,17 @@
 package pe.com.mycow.models;
 
+import java.sql.Connection;
+
 public class BaseEntity {
+  
+      private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+
+        this.connection = connection;
+    }
 }
