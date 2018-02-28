9b10aa8b480e2e30e0955737f92ace4513d712e6
HildaQ <35432455+HildaQ@users.noreply.github.com>
2018-02-28 18:23:48 -0500

    Update UsersBean.java

--- src/pe/com/mycow/beans/UsersBean.java
+++ src/pe/com/mycow/beans/UsersBean.java
@@ -11,52 +11,75 @@ import java.util.List;
 @Named
 @SessionScoped
 public class UsersBean implements Serializable {
+
     private MycowService service;
     private User user;
 
     public UsersBean(){
+
         service = new MycowService();
     }
 
     public List<User> getUsers(){
+
         return service.findAllUsers();
     }
 
     public User getUser() {
+
         return user;
     }
 
     public void setUser(User user) {
+
         this.user = user;
     }
 
-    public String getName(){
+    public String getFirstName(){
+
         return this.getUser().getFirstName();
     }
 
-    public void setName(String name){
-        this.getUser().setFirstName(name);
+    public void setFirstName(String firstName){
+
+        this.getUser().setFirstName(firstName);
     }
 
-    public String editUser(User user){
-        this.setUser(user);
-        return "success";
+    public String getEmail(){
+
+        return this.getUser().getEmail();
     }
+
+    public void setEmail(String email){
+
+        this.getUser().setEmail(email);
+    }
+
+
+    public String getPassword(){
+
+        return this.getUser().getPassword();
+    }
+
+    public void setPassword(String password){
+
+        this.getUser().setPassword(password);
+    }
+
+
     public String newUser(){
         this.setUser(new User());
         return "success";
     }
+
     public  String createUser(){
-        service.createUser(this.getName());
-        return "success";
-    }
-    public String updateUser(){
-        service.updateUser(this.getUser());
+        service.createUser(this.getFirstName(), this.getEmail(), this.getPassword());
         return "success";
     }
 
-    public String deleteUser(User user){
-        service.deleteUser(user.getId());
+    public String doValidateInput (){
+        if(!"mycow".equals(getPassword())) return "error";
         return "success";
     }
 }
+
