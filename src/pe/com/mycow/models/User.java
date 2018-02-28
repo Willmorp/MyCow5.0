bbac9878d4cd3675c5235f4d7d21fad0396b567a
HildaQ <35432455+HildaQ@users.noreply.github.com>
2018-02-28 18:25:17 -0500

    Update User.java

--- src/pe/com/mycow/models/User.java
+++ src/pe/com/mycow/models/User.java
@@ -3,41 +3,51 @@ package pe.com.mycow.models;
 public class User {
 
     private int id;
+    private int dni;
     private String firstName;
     private String lastName;
-    private String password;
-    private String email;
     private int age;
-    private int dni;
+    private String email;
+    private String role;
+    private String password;
 
-    public User(int id, String firstName, String lastName, String password, String email, int age, int dni) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.email = email;
-        this.age = age;
-        this.dni = dni;
+
+    public User(int id, int dni, String firstName, String lastName, int age, String email, String role, String password) {
+        this.setId(id);
+        this.setDni(dni);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setEmail(email);
+        this.setRole(role);
+        this.setPassword(password);
     }
-
-
 
     public User() {
     }
 
-    public User(int id, String firstName) {
+    public User(int id, String firstName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
-
+        this.email = email;
+        this.password = password;
     }
 
     public int getId() {
-
         return id;
     }
 
     public User setId(int id) {
         this.id = id;
+        return  this;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public User setDni(int dni) {
+        this.dni = dni;
         return  this;
     }
 
@@ -47,7 +57,7 @@ public class User {
 
     public User setFirstName(String firstName) {
         this.firstName = firstName;
-        return this;
+        return  this;
     }
 
     public String getLastName() {
@@ -56,25 +66,7 @@ public class User {
 
     public User setLastName(String lastName) {
         this.lastName = lastName;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
+        return  this;
     }
 
     public int getAge() {
@@ -83,17 +75,33 @@ public class User {
 
     public User setAge(int age) {
         this.age = age;
-        return this;
+        return  this;
     }
 
-    public int getDni() {
-        return dni;
+    public String getEmail() {
+        return email;
     }
 
-    public User setDni(int dni) {
-        this.dni = dni;
-        return this;
+    public User setEmail(String email) {
+        this.email = email;
+        return  this;
     }
 
+    public String getRole() {
+        return role;
+    }
 
+    public User setRole(String role) {
+        this.role = role;
+        return  this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return  this;
+    }
 }
